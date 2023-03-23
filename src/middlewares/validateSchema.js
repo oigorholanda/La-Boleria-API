8318aff631ea7a953ed3a52e6c7ed4cad72d5173
Igor Holanda <oigorholanda@gmail.com>
2023-03-23 03:52:59 -0300

    fix: joi schema and validator

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -1,10 +1,15 @@
 export default function validateSchema(schema) {
-    return (req, res, next) => {
-        
-        const validation = schema.validate(req.body, {abortEarly: false})
+   return (req, res, next) => {
+      const validation = schema.validate(req.body, { abortEarly: false });
 
-        if (validation.error) return res.status(400).send(validation.error.message)
-        
-        next()
-    }
+      if (validation.error) {
+         if (validation.error.details[0].type === "string.uriCustomScheme") {
+            return res.status(422).send(validation.error.message);
+         } else {
+            return res.status(400).send(validation.error.message);
+         }
+      }
+
+      next();
+   };
 }
--- src/schemas/cakeSchema.js
+++ src/schemas/cakeSchema.js
@@ -1,9 +1,9 @@
 import Joi from "joi"
 
 export const cakeSchema = Joi.object({
-    name: Joi.string().required(),
+    name: Joi.string().min(2).required(),
     price: Joi.number().positive().required(),
-    image: Joi.string().uri().required(),
+    image: Joi.string().uri({ scheme: ['http', 'https'] }).trim().required(),
     description: Joi.string().required()
 })
 
